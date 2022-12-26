709cf6ab829522f4921839983f1df10ce1729761
fosseddy <markfosseddy@gmail.com>
2022-12-26 17:08:55 +0500

    something

--- main.go
+++ main.go
@@ -4,14 +4,38 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"io"
+	"encoding/json"
 )
 
 const addr string = ":3000"
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-type", "application/json")
-		fmt.Fprint(w, `{"message":"hello, world"}`)
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusNotImplemented)
+			return;
+		}
+
+		if r.Header.Get("Content-Type") != "application/json" {
+			w.WriteHeader(http.StatusNotImplemented)
+			return;
+		}
+
+		rbody, _ := io.ReadAll(r.Body)
+		rbodyjson := map[string]any{}
+		json.Unmarshal(rbody, &rbodyjson) // handle error
+
+		wbody := map[string]any{
+			"data": map[string]string{
+				"token": "sadf7as0d9f7a90s7df",
+			},
+		}
+		wbodyjson, _ := json.Marshal(wbody) // handle error
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		w.Write(wbodyjson)
 	})
 
 	fmt.Println("server is listening on", addr)
