5e8befbc91bc29cac68c658dc4bb8604aad2dae3
fosseddy <markfosseddy@gmail.com>
2022-12-29 21:46:48 +0500

    lets try mysql

--- main.go
+++ main.go
@@ -6,13 +6,14 @@ import (
 	"os"
 	"io"
 	"encoding/json"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 const addr string = ":3000"
 
-type handler struct {
-	msg string
-}
+type handler struct {}
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	uri := r.URL.String()
@@ -44,11 +45,48 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write(wbodyjson)
 }
 
+type user struct {
+	id int
+	username string
+	password string
+}
+
 func main() {
-	h := handler{"hello, world\n"}
+	db, err := sql.Open("mysql", "art:123@/auth-server")
+	if err != nil {
+		panic(err)
+	}
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	rows, err := db.Query("select * from user")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	users := []user{}
+	for rows.Next() {
+		u := user{}
+		err := rows.Scan(&u.id, &u.username, &u.password)
+		if err != nil {
+			panic(err)
+		}
+		users = append(users, u)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", users)
+
+	h := handler{}
 	s := http.Server{Addr: addr, Handler: h}
 
 	fmt.Println("server is listening on", s.Addr)
-	err := s.ListenAndServe()
+	err = s.ListenAndServe()
 	fmt.Fprintf(os.Stderr, "failed to start a server: %v\n", err)
 }
