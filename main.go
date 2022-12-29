4d44de0aadb7822b727c4bf5f3ec666263499ffa
fosseddy <markfosseddy@gmail.com>
2022-12-29 17:07:48 +0500

    custom server handler

--- main.go
+++ main.go
@@ -10,35 +10,45 @@ import (
 
 const addr string = ":3000"
 
+type handler struct {
+	msg string
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	uri := r.URL.String()
+	method := r.Method
+
+	if r.Header.Get("Content-Type") != "application/json" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if uri != "/register" && method != http.MethodPost {
+		w.WriteHeader(http.StatusNotImplemented)
+		return
+	}
+
+	rbody, _ := io.ReadAll(r.Body)
+	rbodyjson := map[string]any{}
+	json.Unmarshal(rbody, &rbodyjson) // handle error
+
+	wbody := map[string]any{
+		"data": map[string]string{
+			"token": "sadf7as0d9f7a90s7df",
+		},
+	}
+	wbodyjson, _ := json.Marshal(wbody) // handle error
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(wbodyjson)
+}
+
 func main() {
-	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			w.WriteHeader(http.StatusNotImplemented)
-			return;
-		}
+	h := handler{"hello, world\n"}
+	s := http.Server{Addr: addr, Handler: h}
 
-		if r.Header.Get("Content-Type") != "application/json" {
-			w.WriteHeader(http.StatusNotImplemented)
-			return;
-		}
-
-		rbody, _ := io.ReadAll(r.Body)
-		rbodyjson := map[string]any{}
-		json.Unmarshal(rbody, &rbodyjson) // handle error
-
-		wbody := map[string]any{
-			"data": map[string]string{
-				"token": "sadf7as0d9f7a90s7df",
-			},
-		}
-		wbodyjson, _ := json.Marshal(wbody) // handle error
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusCreated)
-		w.Write(wbodyjson)
-	})
-
-	fmt.Println("server is listening on", addr)
-	err := http.ListenAndServe(addr, nil)
+	fmt.Println("server is listening on", s.Addr)
+	err := s.ListenAndServe()
 	fmt.Fprintf(os.Stderr, "failed to start a server: %v\n", err)
 }
